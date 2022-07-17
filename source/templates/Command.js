9070533fd38ac6fb6654c15c25fc9045953ee807
Dwifte <67983181+DwifteJB@users.noreply.github.com>
2022-07-17 20:15:31 +0100

    unneeded directory

--- source/templates/Command.js
+++
@@ -1,6 +0,0 @@
-module.exports = class Command {
-    constructor(options = {}) {
-        this.name = (options.name ?? this.constructor.name).trim().toLowerCase();
-        this.description = options.description ?? '' // no description;
-    };
-};
